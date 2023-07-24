6353e4092e886076eb0c57b940b5c7bd484956e0
Lars Kiesow <lkiesow@uos.de>
2023-07-24 19:44:23 +0200

    JavaScript format check

--- uosrzidmgr/static/func.js
+++ uosrzidmgr/static/func.js
@@ -1,105 +1,115 @@
 function validateLogin() {
-	const login = document.querySelector('input[name=login]')
-	if (login.value.length < 4) {
-		login.setCustomValidity('Login too short')
-		return;
-	}
-	fetch('/api/exists/' + login.value)
-	.then(response => response.json())
-	.then(exists => {
-		console.debug(`Login already exists: ${exists}`)
-		login.setCustomValidity(exists ? 'User already exists' : '')
-	})
+  const login = document.querySelector("input[name=login]");
+  if (login.value.length < 4) {
+    login.setCustomValidity("Login too short");
+    return;
+  }
+  fetch("/api/exists/" + login.value)
+    .then((response) => response.json())
+    .then((exists) => {
+      console.debug(`Login already exists: ${exists}`);
+      login.setCustomValidity(exists ? "User already exists" : "");
+    });
 }
-
 
 function validateManagementLogin() {
-	const management_login = document.querySelector('input[name=management_login]')
-	if (management_login.value.length < 2) {
-		management_login.setCustomValidity('Login too short')
-		return;
-	}
-	fetch('/api/exists/' + management_login.value)
-	.then(response => response.json())
-	.then(exists => {
-		console.debug(`Login does exists: ${exists}`)
-		management_login.setCustomValidity(exists ? '' : 'User does not exists')
-	})
+  const management_login = document.querySelector(
+    "input[name=management_login]",
+  );
+  if (management_login.value.length < 2) {
+    management_login.setCustomValidity("Login too short");
+    return;
+  }
+  fetch("/api/exists/" + management_login.value)
+    .then((response) => response.json())
+    .then((exists) => {
+      console.debug(`Login does exists: ${exists}`);
+      management_login.setCustomValidity(exists ? "" : "User does not exists");
+    });
 }
-
 
 function verifyAndSetSuggestion(given, family, len) {
-	given += Math.random().toString(36).substr(10);
-	const suggestion = given.substr(0, len) + family;
-	console.debug(`Generated login suggestion: ${suggestion}`)
-	fetch('/api/exists/' + suggestion)
-	.then(response => response.json())
-	.then(exists => {
-		if (exists) {
-			verifyAndSetSuggestion(given, family, len + 1)
-		} else {
-			const login = document.querySelector('input[name=login]')
-			console.debug(`Suggesting login ${suggestion}`)
-			login.value = suggestion
-			validateLogin()
-		}
-	})
+  given += Math.random().toString(36).substr(10);
+  const suggestion = given.substr(0, len) + family;
+  console.debug(`Generated login suggestion: ${suggestion}`);
+  fetch("/api/exists/" + suggestion)
+    .then((response) => response.json())
+    .then((exists) => {
+      if (exists) {
+        verifyAndSetSuggestion(given, family, len + 1);
+      } else {
+        const login = document.querySelector("input[name=login]");
+        console.debug(`Suggesting login ${suggestion}`);
+        login.value = suggestion;
+        validateLogin();
+      }
+    });
 }
-
 
 function suggestLogin() {
-	const given = document.querySelector('input[name=name_given]').value.toLowerCase().replace(/[^a-z]/g, '')
-	const family = document.querySelector('input[name=name_family]').value.toLowerCase().replace(/[^a-z]/g, '')
-	if (given && family) {
-		verifyAndSetSuggestion(given, family, 1)
-	}
+  const given = document
+    .querySelector("input[name=name_given]")
+    .value.toLowerCase()
+    .replace(/[^a-z]/g, "");
+  const family = document
+    .querySelector("input[name=name_family]")
+    .value.toLowerCase()
+    .replace(/[^a-z]/g, "");
+  if (given && family) {
+    verifyAndSetSuggestion(given, family, 1);
+  }
 }
-
 
 function validatePassword() {
-	const password = document.querySelector('input[name=password]')
-	const password_confirm = document.querySelector('input[name=password_confirm]')
-	password_confirm.setCustomValidity(
-		password.value != password_confirm.value
-		? "Passwords Don't Match"
-		: '');
+  const password = document.querySelector("input[name=password]");
+  const password_confirm = document.querySelector(
+    "input[name=password_confirm]",
+  );
+  password_confirm.setCustomValidity(
+    password.value != password_confirm.value ? "Passwords Don't Match" : "",
+  );
 }
 
-addEventListener("DOMContentLoaded", (event) => {
-	// Verify passwords match
-	const password = document.querySelector('input[name=password]')
-	const password_confirm = document.querySelector('input[name=password_confirm]')
-	if (password && password_confirm) {
-		password.onchange = validatePassword;
-		password_confirm.onkeyup = validatePassword;
-	}
+addEventListener("DOMContentLoaded", () => {
+  // Verify passwords match
+  const password = document.querySelector("input[name=password]");
+  const password_confirm = document.querySelector(
+    "input[name=password_confirm]",
+  );
+  if (password && password_confirm) {
+    password.onchange = validatePassword;
+    password_confirm.onkeyup = validatePassword;
+  }
 
-	// Check that login does not already exist
-	const login = document.querySelector('input[name=login]')
-	login?.addEventListener('keyup', validateLogin)
-	login?.addEventListener('change', validateLogin)
+  // Check that login does not already exist
+  const login = document.querySelector("input[name=login]");
+  login?.addEventListener("keyup", validateLogin);
+  login?.addEventListener("change", validateLogin);
 
-	// Check that the management login exists
-	const management_login = document.querySelector('input[name=management_login]')
-	management_login?.addEventListener('keyup', validateManagementLogin)
-	management_login?.addEventListener('change', validateManagementLogin)
+  // Check that the management login exists
+  const management_login = document.querySelector(
+    "input[name=management_login]",
+  );
+  management_login?.addEventListener("keyup", validateManagementLogin);
+  management_login?.addEventListener("change", validateManagementLogin);
 
-	// Suggest login name
-	const given = document.querySelector('input[name=name_given]')
-	const family = document.querySelector('input[name=name_family]')
-	if (given && family) {
-		given.onchange = suggestLogin;
-		family.onchange = suggestLogin;
-	}
+  // Suggest login name
+  const given = document.querySelector("input[name=name_given]");
+  const family = document.querySelector("input[name=name_family]");
+  if (given && family) {
+    given.onchange = suggestLogin;
+    family.onchange = suggestLogin;
+  }
 
-	// Complete invite URL
-	const invite_link = document.getElementById('invite_link')
-	if (invite_link) {
-		invite_link.innerText = window.location.origin + invite_link.getAttribute('data-link')
-	}
+  // Complete invite URL
+  const invite_link = document.getElementById("invite_link");
+  if (invite_link) {
+    invite_link.innerText =
+      window.location.origin + invite_link.getAttribute("data-link");
+  }
 
-	// Mark required fields
-	for (let elem of document.querySelectorAll('*[required]')) {
-		elem.previousElementSibling.innerText += ' *';
-	}
+  // Mark required fields
+  for (let elem of document.querySelectorAll("*[required]")) {
+    elem.previousElementSibling.innerText += " *";
+  }
 });
